983d0b54ed4c52368a4752aa742be4479a26bf63
Mikhail f. Shiryaev <mr.felixoid@gmail.com>
2021-04-09 12:34:42 +0200

    First working version

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Felixoid/go-versions
+
+go 1.16
+
+require github.com/Felixoid/module-dependency-example v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,12 @@
+// Package main provides ...
+package main
+
+import (
+	"fmt"
+	"github.com/Felixoid/module-dependency-example/structure"
+)
+
+func main() {
+	s := structure.Structure{}
+	fmt.Print(s)
+}
